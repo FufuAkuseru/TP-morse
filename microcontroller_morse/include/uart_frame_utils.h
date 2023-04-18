49f0b471d5490a07b3cce1dc6c285c7461a4e729
Axel FEFEU <fefeuaxel.contact@gmail.com>
2023-04-18 10:36:43 +0200

    forgot to remove something

--- microcontroller_morse/include/uart_frame_utils.h
+++ microcontroller_morse/include/uart_frame_utils.h
@@ -22,17 +22,4 @@ typedef struct uart_frame_s {
 /// @param frame            The frame to write into
 void decode_frame(uint8_t *usart2_rx_buffer, uart_frame_t *frame);
 
-/// @brief Data structure for a queue of UART Frame
-typedef struct uart_frame_queue_s {
-    uart_frame_t array[MAX_QUEUE_SIZE];
-    uint8_t      index_front;
-    uint8_t      index_rear;
-    uint8_t      size;
-} uart_frame_queue_t;
-
-
-/// @brief Initializes a queue of UART Frames
-/// @param uart_frame_queue The queue to initialize
-void init_queue(uart_frame_queue_t *uart_frame_queue);
-
 #endif
